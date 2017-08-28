a7789a0d34d9f6234011feb8eed1ccadc0099c48
Kiba Gateaux <kibagateaux@gmail.com>
2017-08-28 09:32:56 +0200

    adds screens directory

---
+++ src/lib/constants/facebook.js
@@ -0,0 +1,2 @@
+import * as firebase from 'firebase';
+const FBAuthProvider = new firebase.auth.FacebookAuthProvider();
---
+++ src/screens/auth/LoginScreen.js
@@ -0,0 +1,25 @@
+import React from 'react';
+import {
+  View,
+  Text,
+} from 'react-native'
+
+import * as firebase from 'firebase';
+export default class LoginScreen extends Component {
+  constructor(props) {
+    super(props);
+  }
+
+  _authenticate = (provider) => {
+    firebase.
+  }
+
+
+
+  render() {
+    return (
+      <View>
+      </View>
+    )
+  }
+}
