0335810bb52daca647bd5005996d1c412d98530f
Kiba Gateaux <kibagateaux@gmail.com>
2017-10-20 14:20:16 -0400

    adds scrollspy

--- docs/public/js/index.js
+++ docs/public/js/index.js
@@ -1,8 +1,3 @@
 $(document).ready(function(){
-  var _helpers = require('./helpers');  
-  console.log('on load', _helpers);
-  $('a').on('click', (e) => {
-    e.preventDefault();
-    console.log('link clk', e);
-  });
+  $('.scrollspy').scrollSpy();
 });
